042183b7f6a515e05b7bf8a9b40ccefdc49ab52d
LuizNetcer <luizhenrique_netcer@hotmail.com>
2023-04-14 16:36:04 -0300

    criando class livro

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -6,8 +6,42 @@ public class primeiroprograma {
     public static void main(String[] args) {
         gato gato = new gato();
         System.out.println(gato);
+        livro livro = new livro("o problema dos tres corpos", 300);
         /*int a = 5;
         int b = 3;
         System.out.println("hello world! " + (a+b));*/
+    }
+}
+class livro{
+    private string nome;
+    private integer numpag;
+
+    public livro(string nome, integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome=" + nome +
+                ", numpag=" + numpag +
+                '}';
     }
 }
